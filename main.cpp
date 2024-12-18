34682ee3825d7cb763b577543cef8e98c07d8c82
Rusprac <kostiluik0909@gmail.com>
2024-12-18 09:38:16 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
-#include<string>
+#include <cmath>
+using namespace std;
 
 
-float s(float a, float b, float c) {
-    return a + b + c;
+void resh_fales(float a, float b, float c) {
+    if (a <= 0 || b <= 0 || c <= 0) {
+        cout << "Стороны треугольника должны быть положительными" << endl;
+        
+        return;
+    }
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        cout << "Треугольник с такими сторонами не существует" << endl;
+        
+        return;
+    }
+    float p = (a + b + c) / 2;
+    float s = sqrt(p * (p - a) * (p - b) * (p - c));
+
+    cout << "Площадь треугольника: " << s << endl;
 }
-int main() {
-    float d,e,f;
-    std::cout << "Введите три целых числа: "<<std::endl;
-    std::cin >> d >> e >> f;
-    float r = s(d,e,f);
-    std::cout << "Сумма: " << r << std::endl;
 
+int main() {
+    float a, b, c;
+    cout << "Введите значения 3-х сторон: " << endl;
+    cin >> a >> b >> c;
+    resh_fales(a, b, c);
+    
     return 0;
 }
